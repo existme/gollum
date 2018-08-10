d09a5447c15dcb9dbbc02b9a469de56b7ad73c51
Reza Shams Amiri <rezasa@axis.com>
2018-08-10 02:21:43 +0200

    fixed plantUml filter bug

--- assets/rb/plant-filter.rb
+++ assets/rb/plant-filter.rb
@@ -7,48 +7,41 @@ class Gollum::Filter::PlantUML2 < Gollum::Filter
   # placeholders.
   def extract(data)
     return data if @markup.format == :txt
-    # ap data
-    # data.gsub!(/^([ \t]*)(```+) ?([^\r\n]+)?\r?\n(.+?)\r?\n\1(```+)[ \t\r]*$/m) do
-    #     m_indent = Regexp.last_match[1]
-    #     m_start  = Regexp.last_match[2] # ```
-    #     m_lang   = Regexp.last_match[3]
-    #     m_code   = Regexp.last_match[4]
-    #     m_end    = Regexp.last_match[5] # ```
-    #     next '' if m_start.length != m_end.length
-    #     lang = m_lang ? m_lang.strip : nil
-    #     if lang=="sequence" || lang=="uml"
-    #         "#{m_indent}#{replace_plantuml_tags(lang, m_code, m_indent)}"
-    #     else
-    #         "#{m_indent}#{replace_code_tags(lang, m_code, m_indent)}"
-    #     end
-    # end
-    data.gsub!(/^([ \t]*)(~~~+) ?([^\r\n]+)?\r?\n(.+?)\r?\n\1(~~~+)[ \t\r]*$/m) do
-        m_indent = Regexp.last_match[1]
-        m_start  = Regexp.last_match[2] # ~~~
-        m_lang   = Regexp.last_match[3]
-        m_code   = Regexp.last_match[4]
-        m_end    = Regexp.last_match[5] # ~~~
-        next '' if m_start.length != m_end.length
-        lang = m_lang ? m_lang.strip : nil
-        if lang=="sequence" || lang=="uml"
-            "#{m_indent}#{replace_plantuml_tags(lang, m_code, m_indent)}"
-        end
-    end
+
+    extract_lang(data, 'sequence|uml')
     return data
   end
 
-  def replace_plantuml_tags(language, code, indent = "")
+  def replace_plantuml_tags(language, code, indent = '')
     language = language.to_s.empty? ? nil : language
+    p "@startuml\n#{code}\n@enduml\n"
     "@startuml\n#{code}\n@enduml\n"
   end
 
-  def replace_code_tags(language, code, indent = "")
+  def replace_code_tags(language, code, indent = '')
     language = language.to_s.empty? ? nil : language
-    puts "~~~#{language}\n#{code}\n~~~\n"
     "~~~#{language}\n#{code}\n~~~\n"
   end
+
   def process(data)
     data
+  end
+
+  private
+
+  def extract_lang(data, lang_tags)
+    extract_code_lang(data, '~~~', lang_tags)
+    extract_code_lang(data, '```', lang_tags)
+  end
+
+  def extract_code_lang(data, code_tag, lang_tags)
+    data.gsub!(/^([ \t]*)(#{code_tag}) ?(#{lang_tags})$([\S\s]*?)#{code_tag}[ \t\r]*$/m) do
+      m_indent = Regexp.last_match[1]
+      m_lang = Regexp.last_match[3]
+      m_code = Regexp.last_match[4]
+      lang = m_lang ? m_lang.strip : nil
+      "#{m_indent}#{replace_plantuml_tags(lang, m_code, m_indent)}"
+    end
   end
 
 end

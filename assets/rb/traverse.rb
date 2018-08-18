f656ae40651346de374dbf23cde90d838d56941e
Reza Shams Amiri <rezasa@axis.com>
2018-08-18 02:59:00 +0200

    fixed traverse bug
    do not display pages starting with \" this is mainly for pages which
    have unicode characters

--- assets/rb/traverse.rb
+++ assets/rb/traverse.rb
@@ -9,7 +9,7 @@ module Gollum
         # puts [obj.to_s.red,obj.inspect].join(" : ".blue)
         # puts "\n.........................\n"
         obj.instance_variables.map do |var|
-          puts [var.to_s.red, obj.instance_variable_get(var)].join(" : ".blue)
+          puts [var.to_s.red, obj.instance_variable_get(var)].join(' : '.blue)
           puts obj.instance_variable_get(var).to_yaml[0..200].green
         end
         # puts obj.to_yaml
@@ -17,7 +17,7 @@ module Gollum
         # ap data
       end
 
-      def render(title = "Global Table of Contents")
+      def render(_title = 'Global Table of Contents')
         # dump(@wiki)
         render_file
       end
@@ -26,30 +26,30 @@ module Gollum
         new_sub_folder folder_path
       end
 
-      def new_sub_folder(path,folder)
-        %Q(<li class="folder" data-href="#{folder}">#{path}<ul>)
+      def new_sub_folder(path, folder)
+        %(<li class="folder" data-href="#{folder}">#{path}<ul>)
       end
 
       def enclose_tree(string)
-        %Q(<div id="tree"><ul id="treeData" style="display:none" class="tree">\n) + string + %Q(</ul></div>)
+        %(<div id="tree"><ul id="treeData" style="display:none" class="tree">\n) + string + %(</ul></div>)
       end
 
       def new_page(page)
         name = ::File.basename(page.path)
-        url, valid_page  = url_for_page page
+        url, valid_page = url_for_page page
         path = page.path
-        if path.end_with?(".md")
-          %Q(<li data-href="#{url}" id="#{path}" data-icon="md">#{name}</li>)
+        if path.end_with?('.md')
+          %(<li data-href="#{url}" id="#{path}" data-icon="md">#{name}</li>)
         elsif path =~ /^.*(\.png|\.jpg|\.gif)$/
-          %Q(<li data-href="#{url}" id="#{path}" data-icon="png">#{name}</li>)
+          %(<li data-href="#{url}" id="#{path}" data-icon="png">#{name}</li>)
         else
-          %Q()
+          %(<li data-href="#{url}" id="#{path}" data-icon="un">#{name}</li>)
         end
         # %Q(  <li class="file"><a href="#{url}">xxx#{name}</a>#{valid_page ? "" : delete_file(url, valid_page)}</li>)
       end
 
-      def end_folder(path)
-        "</ul></li>"
+      def end_folder(_path)
+        '</ul></li>'
       end
 
       def url_for_page(page)
@@ -57,12 +57,12 @@ module Gollum
         valid_page_name = false
         # Remove ext for valid pages.
         filename = page.filename
-        valid_page_name =  Page::valid_page_name?(filename)
+        valid_page_name = Page.valid_page_name?(filename)
         filename = valid_page_name ? filename.chomp(::File.extname(filename)) : filename
 
         url = ::File.join(::File.dirname(page.path), filename)
         url = url[2..-1] if url[0, 2] == './'
-        return url, valid_page_name
+        [url, valid_page_name]
       end
 
       def render_file
@@ -75,22 +75,22 @@ module Gollum
         count.times do |index|
           page = @pages[index]
           path = page.path
-
-          unless path.include? '/'
-            # Page processed (not contained in a folder)
-            html += "<p>"+path + "</p><br>"
-          else
+          if path.include? '/'
             # Folders start at the next index
             # html += "<p>"+index.inspect+"</p><br>"
             folder_start = index
             break # Pages finished, move on to folders
+          else
+            # Page processed (not contained in a folder)
+            html += '<p>' + path + '</p><br>'
           end
         end
-        ignoredList = Regexp.new('.*(assets)\/.*').freeze
+        ignored_list = Regexp.new('.*(assets)\/.*').freeze
         sorted_folders = []
-        (folder_start).upto count - 1 do |index|
+        folder_start.upto count - 1 do |index|
           path = @pages[index].path
-          if !ignoredList.match(path)
+          unless ignored_list.match(path) || path.start_with?('"') || path.start_with?('.')
+            p path
             sorted_folders += [[path, index]]
           end
         end
@@ -102,7 +102,7 @@ module Gollum
 
           # use :: operator because gollum defines its own conflicting File class
           dir_compare = ::File.dirname(a) <=> ::File.dirname(b)
-
+          # p (dir_compare.inspect)+" : "+::File.dirname(a)+" ? "+::File.dirname(b)
           # Sort based on directory name unless they're equal (0) in
           # which case sort based on file name.
           if dir_compare == 0
@@ -123,27 +123,24 @@ module Gollum
           folder = ::File.dirname path
 
           # Flattern root folder ./Home.md -> Home.md
-          if folder=="."
-            folder=""
-          end
+          folder = '' if folder == '.'
 
           tmp_array = folder.split '/'
-          cur=""
+          cur = ''
           (0...tmp_array.size).each do |index|
-            cur +="/" unless cur==""
+            cur += '/' unless cur == ''
             cur += tmp_array[index]
             if cwd_array[index].nil? || changed
-              html += new_sub_folder(tmp_array[index],cur)
+              html += new_sub_folder(tmp_array[index], cur)
               next
             end
 
-            if cwd_array[index] != tmp_array[index]
-              changed = true
-              (cwd_array.size - index).times do
-                html += end_folder tmp_array[index]
-              end
-              html += new_sub_folder(tmp_array[index],cur)
+            next unless cwd_array[index] != tmp_array[index]
+            changed = true
+            (cwd_array.size - index).times do
+              html += end_folder tmp_array[index]
             end
+            html += new_sub_folder(tmp_array[index], cur)
           end
 
           html += new_page page

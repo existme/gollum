294308dbfdab0e955c5a696e43dc4e22f72bfd70
Reza Shams Amiri <rezasa@axis.com>
2018-08-20 00:31:37 +0200

    fixed#11: when renaming files makesure that folders are created

--- assets/bin/cmd-rename-file.sh
+++ assets/bin/cmd-rename-file.sh
@@ -1,18 +1,25 @@
 #!/bin/sh
 # Renames a folder from $1 to $2 and commits it to the git repo
-
 from=$1
 to=$2
 repo=$3
-
-echo "rename file from $from to $to"
-echo "Root is $repo"
 cd $repo
-mv "./$from" "./$to"
 
+DIR=$(dirname ".$to")
+
+mkdir -p -v $DIR
+ls -la $cwd/$DIR
+
+echo "Created folder $DIR"
+echo "rename file from .$from to .$to"
+echo "Root is $repo"
+
+mv ".$from" ".$to"
+ls -la ".$from"
+ls -la ".$to"
 #assets/bin/move_link.pl $1 $2
 
-git add "./$to"
-git add "./$from"
-git commit -m "Folder renamed from $from to $to"
+git add ".$to"
+git add ".$from"
+git commit -m "File renamed from .$from to .$to"
 #git push

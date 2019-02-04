d06daa2ad097ef5d241026d2b37b6fff382906a8
Reza Shams Amiri <existme@gmail.com>
2019-02-04 14:50:54 +0100

    bumped the version

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@
 
 npm run build:dev # for dev environment, for production: npm run build:prod
 gem build gollum.gemspec
-sudo gem install gollum-4.1.3.gem --no-document
+sudo gem install gollum-4.1.4.gem --no-document
